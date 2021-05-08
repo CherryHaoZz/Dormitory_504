e3d78b0bcc2734df824368e918e8bc9d22e8aa73
hao <haozhiquan5466@live.com>
2021-05-08 11:03:07 +0800

    To(21.5.8): Tmp commit

---
+++ HaoZq/test/move.c
@@ -0,0 +1,11 @@
+
+int main()
+{
+    unsigned int num = 15;
+
+    
+    unsigned int sub1 = num / 2;
+    unsigned int sub2 = num >> 1;
+    unsigned int sub3 = num / 4;
+    unsigned int sub4 = num >> 2;
+}
---
+++ HaoZq/test/move.md
@@ -0,0 +1,7 @@
+```asm
+	shrl	$1, %ecx
+```
+
+'shrl' meant move $x bit to left
+All division operation with 2^n will translate to shrl while source file compile
+
